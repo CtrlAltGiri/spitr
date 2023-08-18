82ded9715d8975ddfcc82796fcda62788223908d
Giridhar Balachandran <giridharb@spitr.com>
2023-08-19 00:37:37 +0530

    Adding dockerfile for fe service

--- fe/myflaskapp/__init__.py
+++ fe/myflaskapp/__init__.py
@@ -9,16 +9,15 @@ app.config.from_envvar('FE_SETTINGS')
 
 @app.before_request
 def before_request():
-    if not g.engine:
-        url = URL.create(
-            drivername="postgresql",
-            username= app.config["POSTGRES_USERNAME"],
-            password= app.config["POSTGRES_PASSWORD"],
-            host= app.config["POSTGRES_HOST"],
-            port= app.config["POSTGRES_PORT"],
-            database= app.config["POSTGRES_DATABASE"]
-        )
-        g.engine = create_engine(url)
+    url = URL.create(
+        drivername="postgresql",
+        username= app.config["POSTGRES_USERNAME"],
+        password= app.config["POSTGRES_PASSWORD"],
+        host= app.config["POSTGRES_HOST"],
+        port= app.config["POSTGRES_PORT"],
+        database= app.config["POSTGRES_DATABASE"]
+    )
+    g.engine = create_engine(url)
 
 app.register_blueprint(homepage)
 app.register_blueprint(users)
--- fe/run.ps1
+++ fe/run.ps1
@@ -1,4 +1,3 @@
 ./env/Scripts/activate
 $env:FE_SETTINGS = "./settings.cfg"
-$env:FLASK_APP = 'run.py'
 flask run
